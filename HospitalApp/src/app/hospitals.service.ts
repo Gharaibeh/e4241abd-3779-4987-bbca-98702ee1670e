d096b6dbc0a4e0454cc1feb78b0a66870b007262
Hasan Gharaibeh <swehu1988@gmail.com>
2021-04-11 21:16:12 +1000

    serilize but need to be pretified

--- HospitalApp/src/app/hospitals.service.ts
+++ HospitalApp/src/app/hospitals.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+interface Hospital {
+  id: string;
+  name: string;
+  location: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class HospitalsService {
@@ -23,6 +30,17 @@ export class HospitalsService {
     { observe: 'response' }).pipe(tap(res => {
       this.retrieve_pagination_links(res);
     }));*/
-    return this.httpClient.get(`${this.apiURL}/hospitals`);
+    // return this.httpClient.get<Hospital[]>(`${this.apiURL}/hospitals`);
+
+    /*return this.httpClient
+      .post(url, JSON.stringify(Hospital), { headers: headers })
+      .pipe(map((res: Response) => res.text()))
+      .subscribe((data) => {
+        console.log(data);
+      });*/
+
+    return this.httpClient.get(`${this.apiURL}/hospitals`, {
+      observe: 'response',
+    });
   }
 }
--- HospitalApp/src/app/user/user.component.ts
+++ HospitalApp/src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HospitalsService } from '../hospitals.service';
+interface Hospital {
+  id: string;
+  name: string;
+  location: string;
+  waitingList: any;
+}
 
 @Component({
   selector: 'app-user',
@@ -9,8 +15,10 @@ import { HospitalsService } from '../hospitals.service';
 export class UserComponent implements OnInit {
   title = 'List of users';
   hospitals;
+  hosiptalHTTP: any;
   constructor(private service: HospitalsService) {
     this.hospitals = service.getHospitals();
+
     // console.log(service.PrintResult());
   }
 
@@ -20,11 +28,22 @@ export class UserComponent implements OnInit {
 
   getFoods() {
     this.service.PrintResult().subscribe(
-      (data) => {
+      (data: any) => {
         console.log(data);
+        // this.hosiptalHTTP = data;
+        console.log(data['body']['page']);
+        //  console.log();
+        for (const element of data['body']['_embedded']['hospitals']) {
+          console.log(element.name);
+        }
+        /* const ll = JSON.stringify(data);
+        for (const element of ll) {
+          console.log(element);
+        }*/
       },
-      (err) => console.error(err),
-      () => console.log('done loading foods')
+      (err: any) => console.error(err),
+      () => console.log('done loading data')
     );
   }
 }
+// var jsonPretty = JSON.stringify(JSON.parse(jsonString),null,2);
