4ef189f646d0cf79a271c93d11c85ed976e2ae8e
Hasan Gharaibeh <swehu1988@gmail.com>
2021-04-12 17:03:22 +1000

    Added level of illness component

---
+++ HospitalApp/src/app/illness/level/level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-level',
+  templateUrl: './level.component.html',
+  styleUrls: ['./level.component.css'],
+})
+export class LevelComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    console.log('susaer is s');
+  }
+}
