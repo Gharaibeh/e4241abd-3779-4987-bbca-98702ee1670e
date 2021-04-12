3755d4807aad6864dee72ee9677aa5f1cc2a1168
Hasan Gharaibeh <swehu1988@gmail.com>
2021-04-12 17:05:24 +1000

    illness level added

--- HospitalApp/src/app/illness/illness.component.ts
+++ HospitalApp/src/app/illness/illness.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-illness',
   templateUrl: './illness.component.html',
-  styleUrls: ['./illness.component.css']
+  styleUrls: ['./illness.component.css'],
 })
 export class IllnessComponent implements OnInit {
+  navLinks: string[] = [];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.navLinks.push('./level');
   }
-
 }
